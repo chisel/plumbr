c1e806848de2e461004e8f453c849b838a20b3b6
Ramtin Soltani <ramtinsoltani93@gmail.com>
2021-05-22 00:19:49 -0700

    optimized and improved stackable directive for better ux.

--- src/app/directives/stackable.directive.ts
+++ src/app/directives/stackable.directive.ts
@@ -1,9 +1,16 @@
-import { Directive, Input, Output, ElementRef, OnInit, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Directive,
+  Input,
+  Output,
+  ElementRef,
+  OnInit,
+  EventEmitter
+} from '@angular/core';
 
 @Directive({
   selector: '[appStackable]'
 })
-export class StackableDirective implements OnInit, OnChanges {
+export class StackableDirective implements OnInit {
 
   @Input('appStackable')
   public stackable: boolean;
@@ -27,6 +34,9 @@ export class StackableDirective implements OnInit, OnChanges {
   @Input('currentScale')
   public currentScale: number = 1;
 
+  @Input('movingZindex')
+  public movingZindex: number = 2000;
+
   @Output('onmovestart')
   public onMoveStart = new EventEmitter<HTMLElement>();
 
@@ -42,24 +52,15 @@ export class StackableDirective implements OnInit, OnChanges {
   private _indexBefore: number = -1;
   private _indexAfter: number = -1;
   private _lastClientY: number = 0;
+  private _helperElement: HTMLElement;
+  private _originalStyles: Partial<CSSStyleDeclaration>;
+  private _mousedownHandler: (event: MouseEvent) => void;
+  private _mousemoveHandler: (event: MouseEvent) => void;
+  private _mouseupHandler: (event: MouseEvent) => void;
 
   constructor(
     private _ref: ElementRef<HTMLElement>
   ) { }
-
-  ngOnChanges(changes: SimpleChanges) {
-
-    // If change is not related to appStackable value, ignore
-    if ( ! changes.stackable ) return;
-
-    // Refresh everything
-    if ( ! this.stackable && changes.stackable.previousValue && this._currentlyMoving ) {
-
-      this.onRefresh.emit();
-
-    }
-
-  }
 
   ngOnInit() {
 
@@ -69,143 +70,165 @@ export class StackableDirective implements OnInit, OnChanges {
       const child: HTMLElement = <any>this._ref.nativeElement.children.item(i);
 
       // Preserve original styles
-      const original = {
+      this._originalStyles = {
         width: child.style.width,
         height: child.style.height,
         position: child.style.position,
         margin: child.style.margin,
         top: child.style.top,
-        left: child.style.left
+        left: child.style.left,
+        zIndex: child.style.zIndex
       };
 
       // Drag start
-      child.addEventListener('mousedown', event => {
-
-        // Skip if directive disabled
-        if ( ! this.stackable ) return;
-
-        event.stopImmediatePropagation();
-
-        // Set as currently moving
-        this._currentlyMoving = child;
-
-        // Create placeholder
-        this._currentPlaceholder = document.createElement('div');
-
-        this._currentPlaceholder.classList.add('stackable-placeholder');
-        this._currentPlaceholder.style.width = window.getComputedStyle(child).width;
-        this._currentPlaceholder.style.height = window.getComputedStyle(child).height;
-        this._currentPlaceholder.style.margin = window.getComputedStyle(child).margin;
-
-        // Adjust styles
-        child.style.width = `calc(${window.getComputedStyle(child).width} + ${this.widthAdjustment})`;
-        child.style.height = `calc(${window.getComputedStyle(child).height} + ${this.heightAdjustment})`;
-        child.style.left = child.offsetLeft + 'px';
-        child.style.top = child.offsetTop + 'px';
-        child.style.margin = '0';
-        child.style.position = 'absolute';
-
-        // Add classes
-        child.classList.add('stackable-moving-child');
-        child.parentElement.classList.add('stackable-moving-parent');
-
-        // Add placeholder
-        child.parentElement.insertBefore(this._currentPlaceholder, child);
-
-        // Emit event
-        this.onMoveStart.emit(child);
-
-      });
+      this._mousedownHandler = event => this.startMove.bind(this)(event, child);
+      child.addEventListener('mousedown', this._mousedownHandler);
 
       // Drag end
-      child.addEventListener('mouseup', event => this.finalizeMove(event, child, this._currentPlaceholder, original));
-      child.addEventListener('mouseleave', event => this.finalizeMove(event, child, this._currentPlaceholder, original));
+      this._mouseupHandler = event => this.finalizeMove.bind(this)(event, child);
+      child.addEventListener('mouseup', this._mouseupHandler);
 
       // Drag
-      child.addEventListener('mousemove', event => {
-
-        const lastY = this._lastClientY;
-        this._lastClientY = event.clientY;
-
-        // Skip if directive disabled
-        if ( ! this.stackable ) return;
-
-        event.stopImmediatePropagation();
-
-        // Skip if not currently moving
-        if ( this._currentlyMoving !== child ) return;
-
-        // Move element on Y-axis
-        child.style.top = (+child.style.top.replace('px', '') + ((event.clientY - lastY) / this.currentScale)) + 'px';
-
-        // Check if center point has crossed any other siblings
-        let children: ChildPosition[] = [];
-
-        // Build a mapping from all children of parent (including currently moving)
-        for ( let j = 0; j < child.parentElement.children.length; j++ ) {
-
-          const ref: HTMLElement = <any>child.parentElement.children.item(j);
-
-          // Skip if looking at placeholder
-          if ( ref === this._currentPlaceholder ) continue;
-
-          children.push({
-            point: ((ref.offsetHeight + +window.getComputedStyle(ref).marginTop.replace('px', '') + +window.getComputedStyle(ref).marginBottom.replace('px', '')) / 2) + ref.offsetTop,
-            ref
-          });
-
-        }
-
-        // Store currently moving child's index
-        const indexBeforeSort = children.findIndex(value => value.ref === child);
-
-        // Sort by center points
-        children = children.sort((a, b) => a.point - b.point);
-
-        // Find index of currently moving child after sort
-        const indexAfterSort = children.findIndex(value => value.ref === child);
-
-        // If child has not been moved enough to change the order, ignore
-        if ( indexBeforeSort === indexAfterSort ) {
-
-          // If placeholder was moved before, restore it to its original state
-          if ( this._placeholderMoved ) {
-
-            child.parentElement.removeChild(this._currentPlaceholder);
-            child.parentElement.insertBefore(this._currentPlaceholder, child);
-
-            this._placeholderMoved = false;
-            this._indexBefore = -1;
-            this._indexAfter = -1;
-
-          }
-
-          return;
-
-        }
-
-        // Otherwise, move the placeholder to the new position
-        child.parentElement.removeChild(this._currentPlaceholder);
-
-        if ( indexAfterSort === children.length - 1 )
-          child.parentElement.appendChild(this._currentPlaceholder);
-        else
-          child.parentElement.insertBefore(this._currentPlaceholder, children[indexAfterSort + 1].ref);
-
-        // Mark placeholder as moved
-        this._placeholderMoved = true;
-
-        // Set indices
-        this._indexBefore = indexBeforeSort;
-        this._indexAfter = indexAfterSort;
-
-      });
+      this._mousemoveHandler = event => this.moveElement.bind(this)(event, child);
+      child.addEventListener('mousemove', this._mousemoveHandler);
 
     }
 
   }
 
-  public finalizeMove(event: MouseEvent, child: HTMLElement, placeholder: HTMLElement, original: any) {
+  public startMove(event: MouseEvent, child: HTMLElement) {
+
+    // Skip if directive disabled
+    if ( ! this.stackable ) return;
+
+    event.stopImmediatePropagation();
+
+    // Set as currently moving
+    this._currentlyMoving = child;
+
+    // Create placeholder
+    this._currentPlaceholder = document.createElement('div');
+
+    this._currentPlaceholder.classList.add('stackable-placeholder');
+    this._currentPlaceholder.style.width = window.getComputedStyle(child).width;
+    this._currentPlaceholder.style.height = window.getComputedStyle(child).height;
+    this._currentPlaceholder.style.margin = window.getComputedStyle(child).margin;
+
+    // Create an element to capture mousemove outside of the target element
+    this._helperElement = document.createElement('div');
+    this._helperElement.style.width = '100vw';
+    this._helperElement.style.height = '100vh';
+    this._helperElement.style.position = 'absolute';
+    this._helperElement.style.backgroundColor = 'transparent';
+    this._helperElement.style.cursor = 'grabbing';
+    this._helperElement.style.zIndex = this.movingZindex + ' !important';
+    this._helperElement.onmouseup = event => this.finalizeMove.bind(this)(event, child);
+    this._helperElement.onmousemove = event => this.moveElement.bind(this)(event, child);
+
+    // Adjust styles
+    child.style.width = `calc(${window.getComputedStyle(child).width} + ${this.widthAdjustment})`;
+    child.style.height = `calc(${window.getComputedStyle(child).height} + ${this.heightAdjustment})`;
+    child.style.left = child.offsetLeft + 'px';
+    child.style.top = child.offsetTop + 'px';
+    child.style.margin = '0';
+    child.style.position = 'absolute';
+    child.style.zIndex = (this.movingZindex + 1) + ' !important';
+
+    // Add classes
+    child.classList.add('stackable-moving-child');
+    child.parentElement.classList.add('stackable-moving-parent');
+
+    // Add placeholder
+    child.parentElement.insertBefore(this._currentPlaceholder, child);
+
+    // Add helper element
+    document.body.append(this._helperElement);
+
+    // Emit event
+    this.onMoveStart.emit(child);
+
+  }
+
+  public moveElement(event: MouseEvent, child: HTMLElement) {
+
+    const lastY = this._lastClientY;
+    this._lastClientY = event.clientY;
+
+    // Skip if directive disabled
+    if ( ! this.stackable ) return;
+
+    // Skip if not currently moving
+    if ( this._currentlyMoving !== child ) return;
+
+    event.stopImmediatePropagation();
+
+    // Move element on Y-axis
+    child.style.top = (+child.style.top.replace('px', '') + ((event.clientY - lastY) / this.currentScale)) + 'px';
+
+    // Check if center point has crossed any other siblings
+    let children: ChildPosition[] = [];
+
+    // Build a mapping from all children of parent (including currently moving)
+    for ( let j = 0; j < child.parentElement.children.length; j++ ) {
+
+      const ref: HTMLElement = <any>child.parentElement.children.item(j);
+
+      // Skip if looking at placeholder
+      if ( ref === this._currentPlaceholder ) continue;
+
+      children.push({
+        point: ((ref.offsetHeight + +window.getComputedStyle(ref).marginTop.replace('px', '') + +window.getComputedStyle(ref).marginBottom.replace('px', '')) / 2) + ref.offsetTop,
+        ref
+      });
+
+    }
+
+    // Store currently moving child's index
+    const indexBeforeSort = children.findIndex(value => value.ref === child);
+
+    // Sort by center points
+    children = children.sort((a, b) => a.point - b.point);
+
+    // Find index of currently moving child after sort
+    const indexAfterSort = children.findIndex(value => value.ref === child);
+
+    // If child has not been moved enough to change the order, ignore
+    if ( indexBeforeSort === indexAfterSort ) {
+
+      // If placeholder was moved before, restore it to its original state
+      if ( this._placeholderMoved ) {
+
+        child.parentElement.removeChild(this._currentPlaceholder);
+        child.parentElement.insertBefore(this._currentPlaceholder, child);
+
+        this._placeholderMoved = false;
+        this._indexBefore = -1;
+        this._indexAfter = -1;
+
+      }
+
+      return;
+
+    }
+
+    // Otherwise, move the placeholder to the new position
+    child.parentElement.removeChild(this._currentPlaceholder);
+
+    if ( indexAfterSort === children.length - 1 )
+      child.parentElement.appendChild(this._currentPlaceholder);
+    else
+      child.parentElement.insertBefore(this._currentPlaceholder, children[indexAfterSort + 1].ref);
+
+    // Mark placeholder as moved
+    this._placeholderMoved = true;
+
+    // Set indices
+    this._indexBefore = indexBeforeSort;
+    this._indexAfter = indexAfterSort;
+
+  }
+
+  public finalizeMove(event: MouseEvent, child: HTMLElement) {
 
     // Skip if directive disabled
     if ( ! this.stackable && ! this._currentlyMoving ) return;
@@ -216,22 +239,26 @@ export class StackableDirective implements OnInit, OnChanges {
     if ( this._currentlyMoving !== child ) return;
 
     // Restore styles
-    child.style.width = original.width;
-    child.style.height = original.height;
-    child.style.left = original.left;
-    child.style.top = original.top;
-    child.style.margin = original.margin;
-    child.style.position = original.position;
+    child.style.width = this._originalStyles.width;
+    child.style.height = this._originalStyles.height;
+    child.style.left = this._originalStyles.left;
+    child.style.top = this._originalStyles.top;
+    child.style.margin = this._originalStyles.margin;
+    child.style.position = this._originalStyles.position;
+    child.style.zIndex = this._originalStyles.zIndex;
 
     // Remove classes
     child.classList.remove('stackable-moving-child');
     child.parentElement.classList.remove('stackable-moving-parent');
 
     // Remove placeholder
-    child.parentElement.removeChild(placeholder);
+    child.parentElement.removeChild(this._currentPlaceholder);
+
+    // Remove helper element
+    document.body.removeChild(this._helperElement);
 
     // Remove currently moving ref
-    this._currentlyMoving = null;
+    // this._currentlyMoving = null;
 
     // Emit event
     this.onMoveEnd.emit({
