eaa64ffd062507a004c655a5b1cd60899dc1b0d7
Ramtin Soltani <ramtinsoltani93@gmail.com>
2021-05-28 18:28:04 -0700

    updated the update-version.js script to also commit the changes to version.

--- update-version.js
+++ update-version.js
@@ -4,8 +4,25 @@ const version = process.argv[2]?.trim();
 const swConfig = require(path.resolve(__dirname, 'ngsw-config.json'));
 const packageJson = require(path.resolve(__dirname, 'package.json'));
 const packageLockJson = require(path.resolve(__dirname, 'package-lock.json'));
+const child = require('child_process');
 
 if ( ! version ) throw new Error('No version specified!');
+
+function spawn(command, ...args) {
+
+  return new Promise((resolve, reject) => {
+
+    child.spawn(command, args, {
+      windowsHide: true,
+      cwd: process.cwd(),
+      stdio: 'inherit'
+    })
+    .on('close', resolve)
+    .on('error', reject);
+
+  });
+
+}
 
 swConfig.appData = { version };
 packageJson.version = version;
@@ -25,3 +42,7 @@ fs.writeFileSync(
   path.resolve(__dirname, 'package-lock.json'),
   JSON.stringify(packageLockJson, null, 2)
 );
+
+spawn('git', 'add', 'ngsw-config.json', 'package.json', 'package-lock.json')
+.then(() => spawn('git', 'commit', '-m', `"v${version}"`))
+.catch(console.error);
